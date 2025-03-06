e67640d397d99661e9af5936ac78b26b1cb9006e
stilwellc <cns9383@nyu.edu>
2025-03-06 14:15:52 -0500

    Add GitHub Pages configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/mobi',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
