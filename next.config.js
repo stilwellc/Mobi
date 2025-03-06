3be13c8b8ae090d984db9c8918cdb8a0d779961f
stilwellc <cns9383@nyu.edu>
2025-03-06 14:32:01 -0500

    Update Next.js config and add 404 redirect

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
   assetPrefix: '/mobi/',
   distDir: 'out',
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
