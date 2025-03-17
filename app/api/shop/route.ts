af749694a53d20459f893a3df350ad5fc40d74f9
stilwellc <cns9383@nyu.edu>
2025-03-16 22:54:12 -0400

    Add new shop items: DKR chairs, Castelli chairs, Enetri shelf, and Wassily chairs

--- app/api/shop/route.ts
+++ app/api/shop/route.ts
@@ -11,6 +11,26 @@ const shopItems = [
     id: 'pace-collection-scaffold-chairs',
     title: 'Pace Collection Scaffold Chairs',
     imagePath: '/images/items/Pace-Collection-Scaffold-Chairs.jpg'
+  },
+  {
+    id: '1960s-herman-miller-dkr-chairs-2x',
+    title: '1960s Herman Miller DKR Chairs (2x)',
+    imagePath: '/images/items/1960s-Herman-Miller-DKR-Chairs-(2x).jpg'
+  },
+  {
+    id: 'castelli-dsc-106-chairs-4x',
+    title: 'Castelli DSC 106 Chairs (4x)',
+    imagePath: '/images/items/Castelli-DSC-106-Chairs-(4x).jpg'
+  },
+  {
+    id: 'ikea-enetri-shelf',
+    title: 'Ikea Enetri Shelf',
+    imagePath: '/images/items/Ikea-Enetri-Shelf.jpg'
+  },
+  {
+    id: 'replica-wassily-chairs-2x',
+    title: 'Replica Wassily Chairs (2x)',
+    imagePath: '/images/items/Replica-Wassily-Chairs-(2x).jpg'
   }
 ];
 
